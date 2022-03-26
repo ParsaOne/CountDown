f5fc5cb667709a903f89ed1168ef67b7db4b108d
ParsaOne <93545502+ParsaOne@users.noreply.github.com>
2022-03-26 12:12:04 +0430

    0.0.0.2.0
    Rocket Project
    
    0.0.0.2.0 Added Keyboard Controls ... Fixed some Bugs ...

---
+++ 0.0.0.2.0/script.js
@@ -0,0 +1,53 @@
+let StartBox = document.getElementById('start_box');
+let InputCounter = document.getElementById('input_counter');
+let StartButtom = document.getElementById('start_counter');
+let Board = document.getElementById('Board');
+let circle = document.getElementById('circle');
+let TimerNumber = document.getElementById('TimerNum');
+
+StartButtom.addEventListener('click', function () {
+    Start();
+});
+InputCounter.addEventListener("keydown", function(KeyControl) {
+        switch(KeyControl.code) {
+            case "Enter":
+                Start();
+            break;
+        };
+});
+
+function Start() {
+    let Seconds = parseInt(InputCounter.value);
+    let StartTime = Seconds;
+    let Persent;
+    let LPersent ='';
+    let TimerID;
+    
+    if (isNaN(Seconds)) {
+        WrongInput();
+        return;
+    } else {
+    Board.textContent = ('در حال اجرا ...');
+    circle.style.display = ('Block');
+    TimerNumber.textContent = Seconds;
+    StartBox.style.display = ('None');
+    TimerID = setInterval(() => {
+        if (LPersent) {circle.classList.remove(LPersent);}
+        if ( Seconds <= 0 ) {
+            clearInterval(TimerID);
+            StartBox.style.display = ('flex');
+            Board.textContent = ('تمام شد...');
+            Board.style.color = ('Green');
+            circle.style.display = ('None');
+            InputCounter.value = ('Null');
+            return;}
+        Seconds -= 1;
+        LPersent = (`p${Persent = Math.floor( ( ( StartTime - Seconds ) / StartTime ) * 100 )}`);
+        circle.classList.add(`p${Persent}`);
+        TimerNumber.textContent = Seconds; 
+    }, 1000);}
+};
+function WrongInput() {
+    Board.textContent = ('زمان را به درستی وارد کنید...');
+    Board.style.color = ('Red');
+};
